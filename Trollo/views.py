2c516d1aa2aafcb6df9adc07f27171b15c23c92e
Bartiik <bkudron@gmail.com>
2017-06-04 18:23:57 +0200

    commit

--- Trollo/views.py
+++ Trollo/views.py
@@ -8,4 +8,14 @@ def index(request):
 	template = loader.get_template('Trollo/index.html')
 	context = { 'Object_list': Object_list,}
 	return HttpResponse(template.render(context,request))
-	
+def sax(request):
+    template = loader.get_template('Trollo/SaxGuy.html')
+    return HttpResponse(template.render(1,request))
+def project(request,project_id):
+    id = project_id
+    List_list = List.objects.raw('SELECT * FROM trollo_list WHERE project_id=%s',id)
+    Task_list = Task.objects.raw('SELECT * FROM trollo_task WHERE project_id=%s ORDER BY List_id',id)
+    Element_list = Element.objects.raw('SELECT * FROM Trolo.element WHERE project_id=%s ORDER BY Task_id',id)
+    context = { 'id': id, 'List_list': List_list, 'Task_list': Task_list, 'Element_list':Element_list,}
+    template = loader.get_template('Trollo/project.html')
+    return HttpResponse(template.render(context,request))
